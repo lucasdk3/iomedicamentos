cba845eed905b34ce080785f95a150dba074c8ad
lucasdk3 <lucasbatista1996@hotmail.com>
2020-09-25 19:46:52 -0300

    alterado nome
    Signed-off-by: lucasdk3 <lucasbatista1996@hotmail.com>

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,5 +1,5 @@
 String textSobre =
-    'O iomedicamentos é um aplicativo para auxiliar os profissionais e estudantes de Odontologia a calcular as dosagens terapêuticas de diversos medicamentos conforme idade e/ou peso do paciente. Ele foi idealizado por professores e alunos do Curso de Odontologia do Centro Universitário Unisep (CEUUN)/Campus de Francisco Beltrão-PR, como um projeto de trabalho de conclusão de curso (TCC) em parceria com a Origem Studios.';
+    'O Posologuia Odonto é um aplicativo para auxiliar os profissionais e estudantes de Odontologia a calcular as dosagens terapêuticas de diversos medicamentos conforme idade e/ou peso do paciente. Ele foi idealizado por professores e alunos do Curso de Odontologia do Centro Universitário Unisep (CEUUN)/Campus de Francisco Beltrão-PR, como um projeto de trabalho de conclusão de curso (TCC) em parceria com a Origem Studios.';
 String texto1 =
     'Emyr Stringhini Junior - Professor Doutor da disciplina de Odontologia Pediátrica do Centro Universitário Unisep (CEUUN) – emyr@unisep.edu.br';
 String texto2 =
